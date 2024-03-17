56bd8e2160f74964033c6123aa22608926d2406b
Ilayda Bulut <ilaydabulut201@gmail.com>
2024-03-17 14:49:24 +0300

    Initial commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0xFFD2FFF4);
+const kPrimaryColor = Color(0xFFFFFFFF);
+const kSecondaryColor = Color(0xDBD35D31);
+const kTuruncu = Color(0xFFFF914D);
